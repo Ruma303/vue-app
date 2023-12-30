7af9569df7d96844c8a7a231a0fbe978578e09c1
Ruma303 <matteoh8@hotmail.it>
2023-12-30 14:48:12 +0100

    eventi da completare

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
+import Product from './components/Product.vue';
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="./assets/img/logo.svg" width="125" height="125" />
-
-        <p class="classe">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate dignissimos facilis quod
-            reprehenderit sit, ea animi, fugiat vel voluptatum recusandae esse consequatur possimus repudiandae quaerat
-            architecto dicta ipsum perferendis inventore.</p>
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-            <!-- <hello-world></hello-world>
-            <HelloWorld></HelloWorld> -->
-        </div>
-    </header>
-
     <main>
-        <TheWelcome />
-        <TheWelcome />
-        <TheWelcome />
+        <h2>Quantity {{ quantity }}</h2>
+        <Product
+            component-title="Prodotto"
+            component-description="Descrizione del prodotto"
+            price="20"
+        />
     </main>
 </template>
 
 
 <style scoped>
-.classe {
-    color: red;
-}
-
-header {
-    line-height: 1.5;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-}
 </style>
 
-<!-- <style scoped lang="sass">
-header
-  line-height: 1.5
-  background-color: lightblue
-
-  .classe
-    color: red
-
-  @media (min-width: 1024px)
-    display: flex
-    place-items: center
-    padding-right: calc(var(--section-gap) / 2)
-
-    .wrapper
-      display: flex
-      place-items: flex-start
-      flex-wrap: wrap
-
-.logo
-  display: block
-  margin: 0 auto 2rem
-  @media (min-width: 1024px)
-    margin: 0 2rem 0 0
-</style> -->
---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+    <script setup>
+    const props = defineProps({
+        componentTitle: {
+            type: String,
+            required: true
+        },
+        componentDescription: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            default: 0,
+        }
+    });
+    const methods = {
+        buy() {
+            this.$emit('buy-product');
+        }
+    }
+    </script>
+
+    <template>
+        <h1>{{ componentTitle }}</h1>
+        <p>{{ componentDescription }}</p>
+        <p>{{ price }}€</p>
+        <button @click="$emit('buy', 1)">Buy</button>
+    </template>
+
